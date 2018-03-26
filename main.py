4018bb9d613605f955c618f3ae475ea944355d5f
Daniel Kovacs <dani@taiga.sch.bme.hu>
2018-03-26 23:16:56 +0200

    Partialy working authenticate and send json with pycurl.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import time
 import os
 import logging
 from fuzzer import *
-
+import pycurl
+import json
+from StringIO import StringIO
 ###### Logging ######
 if not os.path.exists('log'):
     os.mkdir('log')
@@ -12,6 +14,34 @@ log = logging.getLogger("Main")
 filename = "log/log_file_" + str(time.ctime()).replace(' ', '_').replace(':', '-') + '.log'
 logging.basicConfig(filename=filename, level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(message)s')
 
+
+def authenticate(url):
+    buffer = StringIO()
+    payload=json.dumps({'password':'asdf@1234', 'type':'normal',"username":"daniel.kovacs"})
+    c=pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.POSTFIELDS,payload)
+    c.setopt(pycurl.HTTPHEADER, ["Content-type: application/json"])
+    c.setopt(c.WRITEDATA, buffer)
+    #print(payload+'\n')
+    c.perform()
+    auth_token=json.loads(buffer.getvalue())['auth_token']
+    statuscode=c.getinfo(pycurl.HTTP_CODE)
+    #print json.loads(buffer.getvalue(), indent=4, separators=(',', ': '))
+    return auth_token
+
+def queryData(token, url, payload):
+    buffer = StringIO()
+    c=pycurl.Curl()
+    c=pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.POSTFIELDS,payload)
+    c.setopt(pycurl.HTTPHEADER, ["Content-type: application/json","Authorization: Bearer "+token])
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    result = buffer.getvalue()
+    statuscode=c.getinfo(pycurl.HTTP_CODE)
+    return result
 
 def main(json, url, iterations):
     # TODO
@@ -29,6 +59,9 @@ if __name__ == "__main__":
         log.info("Webserver URL: " + args.url)
         log.info("Number of iterations: " + args.iterations)
         fuzzer = Fuzzer()
-        main(args.json, args.url, args.iterations)
+        auth_token=authenticate(args.url+'/auth')
+        payload=json.dumps({'description':'Teszt','name':'Teszt Projekt'})
+        queryData(auth_token,args.url+'/projects',payload)
+	#main(args.json, args.url, args.iterations)
     except Exception as e:
         print(e)
