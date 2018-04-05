ec2746649a3ff443b857144c442a05fc169c88de
HuNoble <helloinfi@gmail.com>
2018-04-05 20:25:15 +0200

    test cases added

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,6 +1,8 @@
 import unittest
+import json
+import pycurl
 
-from fuzzer.fuzzer import Fuzzer
+from fuzzer.main import authenticate, queryData, Fuzzer
 
 
 class Test(unittest.TestCase):
@@ -9,6 +11,34 @@ class Test(unittest.TestCase):
         f = Fuzzer('{"a":1}')
         f.fuzz()
         f.fuzz()
+
+    def test_fuzzing(self):
+        fbase = '{"a":1}'
+        f = Fuzzer(fbase)
+        for x in range(5):
+            f2 = f.fuzz()
+            self.assertNotEqual(fbase, f2)
+            fbase = f2
+
+    def test_auth(self):
+        url = "10.152.106.224/api/v1/auth"
+        token = authenticate(url)
+        self.assertNotEqual(token, "")
+        url = "10.152.106.224"
+        with self.assertRaises(json.decoder.JSONDecodeError):
+            authenticate(url)
+        url = "MAX POWER"
+        with self.assertRaises(pycurl.error):
+            authenticate(url)
+
+    def test_query(self):
+        url = "10.152.106.224/api/v1/auth"
+        token = authenticate(url)
+        statuscode, result = queryData(token, url + '/projects', Fuzzer('{"a":1}').fuzz())
+        self.assertNotEqual("", result)
+        print(statuscode)
+        self.assertGreater(int(statuscode), 99)
+        self.assertLess(int(statuscode), 600)
 
 
 if __name__ == '__main__':
