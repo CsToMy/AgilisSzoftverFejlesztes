0657ec926b062229aee61d9a68fb0adec5ac943a
HuNoble <helloinfi@gmail.com>
2018-04-17 11:31:13 +0200

    All test cases are up and running

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,9 +1,7 @@
 import unittest
-# import json
-# import pycurl
-
-# from fuzzer.main import authenticate, queryData, Fuzzer
-from fuzzer.main import Fuzzer
+import json
+import pycurl
+from fuzzer.main import authenticate, queryData, Fuzzer
 
 
 class Test(unittest.TestCase):
@@ -21,25 +19,24 @@ class Test(unittest.TestCase):
             self.assertNotEqual(fbase, f2)
             fbase = f2
 
-    # def test_auth(self):
-    #     url = "10.152.106.224/api/v1/auth"
-    #     token = authenticate(url)
-    #     self.assertNotEqual(token, "")
-    #     url = "10.152.106.224"
-    #     with self.assertRaises(json.decoder.JSONDecodeError):
-    #         authenticate(url)
-    #     url = "MAX POWER"
-    #     with self.assertRaises(pycurl.error):
-    #         authenticate(url)
-    #
-    # def test_query(self):
-    #     url = "10.152.106.224/api/v1/auth"
-    #     token = authenticate(url)
-    #     statuscode, result = queryData(token, url + '/projects', Fuzzer('{"a":1}').fuzz())
-    #     self.assertNotEqual("", result)
-    #     print(statuscode)
-    #     self.assertGreater(int(statuscode), 99)
-    #     self.assertLess(int(statuscode), 600)
+    def test_auth(self):
+        url = "152.66.209.124/api/v1/auth"
+        token = authenticate(url)
+        self.assertNotEqual(token, "")
+        url = "152.66.209.124"
+        with self.assertRaises(json.decoder.JSONDecodeError):
+            authenticate(url)
+        url = "MAX POWER"
+        with self.assertRaises(pycurl.error):
+            authenticate(url)
+
+    def test_query(self):
+        url = "152.66.209.124/api/v1/auth"
+        token = authenticate(url)
+        statuscode, result = queryData(token, url + '/projects', Fuzzer('{"a":1}').fuzz())
+        self.assertNotEqual("", result)
+        self.assertGreater(int(statuscode), 99)
+        self.assertLess(int(statuscode), 600)
 
 
 if __name__ == '__main__':
